add5dc46b2506f269f50968823ebc7ff80a80bea
Alexey Alexandrov <alexei.maui@gmail.com>
2020-09-21 23:07:56 +0300

    add assertFalse

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -175,6 +175,6 @@ class WallServiceTest {
         val result = service.update(update)
 
         // проверяем результат (используйте assertTrue или assertFalse)
-        assertTrue(result)
+        assertFalse(result)
     }
 }
